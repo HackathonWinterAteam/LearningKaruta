43c5afc338fe356058cd34e277ad525d9697dd9c
Mai Miz <m.711029@gmail.com>
2023-01-24 22:42:25 +0900

    最低限のスキーマ定義（仮記述）

--- app/src/schemas/games.py
+++ app/src/schemas/games.py
@@ -2,9 +2,15 @@ from typing import Optional
 from pydantic import BaseModel, Field
 
 
-#レスポンススキーマ(クラス名称は要検討)
+#レスポンススキーマ(クラス名称は要検討)仮記述のため後ほど要検討！
+class Box_list(BaseModel):
+    box_id : int
+    box_name : str
+
 class Box(BaseModel):
-    
+    box_id : int
+    box_name : str
+    card_object : dict[str]
 
 
 #リクエストスキーマ
