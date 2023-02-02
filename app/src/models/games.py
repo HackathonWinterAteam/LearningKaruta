ae544cefb74c8e474ce67971201481ea92f919fe
Mai Miz <m.711029@gmail.com>
2023-02-02 15:33:24 +0900

    モデル作成

--- app/src/models/games.py
+++ app/src/models/games.py
@@ -2,12 +2,13 @@
 # 設計したテーブルをここに落とし込む
 # FK制約追加する
 
-from sqlalchemy import Column, Integer, String, Text, Enum, ForeignKey #他使用するものすべて
+from sqlalchemy import Column, Integer, String, Text, Enum, ForeignKey, Boolean #他使用するものすべて
 from database import engine, Base
 from models.users import #ユーザーモデルのユーザークラスをインポート
 
 #テーブル作成
-class card_boxes(Base):
+
+class boxes(Base):
     __tablename__ = 'card_boxes'
     __table_args__ = {
         'comment': 'お題マスタ'
@@ -50,23 +51,15 @@ class question_answer_cards(Base):
     card_level = Column('card_level', Enum, nullable=True)
 
 
-class box_card(Base):
+class boxes_cards(Base):
     __tablename__ = 'box_card'
     __table_args__ = {
-        'comment': 'お題マスタと札テーブルの中間テーブル'
+        'comment': 'お題と札を紐づける中間テーブル'
     }
 
     box_id = Column('box_id', ForeignKey=card_boxes.box_id)
     card_id = Column('card_id', ForeignKey=question_answer_cards.card_id)
 
-class answer_images(Base):
-    __tablename__ = 'answer_images'
-    __table_args__ = {
-        'comment': '取り札に画像をつけるためのテーブル'
-    }
-
-    answer_image_id = Column('answer_image_id', primary_key=True, index=True)
-    answer_id = Column('answer_id', Text, nullable=True, ForeignKey=answer_texts.answer_id)
 
 class play_records(Base):
     __tablename__ = 'play_records'
@@ -80,3 +73,25 @@ class play_records(Base):
     number_of_corrected = Column('number_of_corrected', Integer, nullable=False)
     played_at = Column('played_at', nullable=False) #TIMESTAMPを定義
     play_type = Column('play_type', Text, nullable=False)
+
+
+class record_details(Base):
+    __tablename__ = 'record_details'
+    __table_args__ = {
+        'comment': 'プレイ記録詳細'
+    }
+
+    card_id = Column('card_id', primary_key=True, ForeignKey = box_card.card_id)
+    played_id = Column('played_id', primary_key=True, ForeignKey = play_records.played_id)
+    judgement = Column('judgement', Boolean, nullable=False)
+    user_id = Column('user_id', ForeignKey=) #ユーザーID持ってくる
+    
+
+class box_type(Base):
+    __tablename__ = 'box_type'
+    __table_args__ = {
+        'comment': 'お題別プレイを選んだ時のお題紐づけ'
+    }
+    played_id = Column('played_id', primary_key=True)
+    box_id = Column('box_id', nullable=False)
+
