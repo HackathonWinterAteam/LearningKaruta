8eede1a517c754cb280e0d6696f40d3b2936eeb6
maimy <m.711029@gmail.com>
2023-02-09 10:54:24 +0900

    接続処理追記

--- app/cruds/games.py
+++ app/cruds/games.py
@@ -1,10 +1,27 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import Session
+from sqlalchemy.sql import text
+from sqlalchemy import bindparam
 from models import games as games_model
 from schemas import games as games_schema
 
-def read_boxes(db: Session):
-    BoxList = db.query(games_model.boxes).all()
+def read_boxes(db: Session, skip: int = 0):
+    BoxList = db.query(games_model.boxes).offset(skip).all()
     return BoxList
 
 
+# 札取得JOINの定義
+'''
+boxes_cards = games_model.boxes_cards
+q_a_cards = games_model.question_answer_cards
+q_texts = games_model.question_texts
+a_texts = games_model.answer_texts
+a_images = games_model.answer_images
+'''
+
+
+def read_playing_cards(db: Session):
+    t = text('SELECT ') #結合抽出処理
+    t = t.bindparams(bindparam("",type=))
+    PlayCardsList = db.execute(t, {"": })
+    return PlayCardsList
--- app/models/games.py
+++ app/models/games.py
@@ -37,6 +37,15 @@ class answer_texts(Base):
     answer_id = Column('answer_id', Integer, primary_key=True, autoincrement=True)
     answer_text = Column('answer_text', Text, nullable=False)
 
+class answer_images(Base):
+    __tablename__ = 'answer_images'
+    __table_args__ = {
+        'comment': '取り札画像パス格納テーブル'
+    }
+
+    answer_image_id = Column('answer_image_id', Integer, primary_key=True, autoincrement=True)
+    answer_id = Column('answer_id', Integer, ForeignKey(answer_texts.answer_id))
+    answer_file_pass = Column('answer_file_pass', Text, nullable=True)
 
 # 札レベルで使用するENUM型の定義
 '''
