64ff48d2a29dbb1ab42765f7bd3727a427e2e3aa
watttaraaru <101541944+wattaraaru@users.noreply.github.com>
2023-02-12 15:13:41 +0900

    各コンポーネントを追加

---
+++ front/src/compornents/Modal.jsx
@@ -0,0 +1,18 @@
+export const Modal = ({ modalClose, children }) => {
+    return (
+      <>
+        <section className="modalArea" onClick={modalClose}>
+          <div className="modalBg"></div>
+          <div className="modalWrapper">
+            <div className="modalContents">
+              <h1>モーダル</h1>
+              <p>{children}</p>
+            </div>
+            <div className="closeModal" onClick={modalClose}>
+              ×
+            </div>
+          </div>
+        </section>
+      </>
+    );
+  };
