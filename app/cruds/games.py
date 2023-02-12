e4d64fe6771359ec29a67826afbc40192e0e2ea1
Sou Yonamine <sasisusesou24@gmail.com>
2023-02-12 11:10:31 +0900

    docker-compose.ymlのコンフリクト解消

--- app/cruds/games.py
+++ app/cruds/games.py
@@ -5,6 +5,9 @@ from sqlalchemy import bindparam
 from models import games as games_model
 from schemas import games as games_schema
 
+
+# お題一覧取得
+
 def read_boxes(db: Session, skip: int = 0):
     BoxList = db.query(games_model.boxes).offset(skip).all()
     return BoxList
@@ -19,9 +22,14 @@ a_texts = games_model.answer_texts
 a_images = games_model.answer_images
 '''
 
-
+'''
 def read_playing_cards(db: Session):
-    t = text('SELECT ') #結合抽出処理
+    t = text('SELECT card_id, question_id, answer_id, question_text, answer_text, answer_file_pass 
+            FROM (question_answer_cards AS qac
+            INNER JOIN question_texts AS qt
+            ON (qac.question_id = qt.question_id AND ))') #結合抽出処理
     t = t.bindparams(bindparam("",type=))
     PlayCardsList = db.execute(t, {"": })
     return PlayCardsList
+
+    '''
