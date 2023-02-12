276d456e0dce9912c6c83df75306692781a8489e
watttaraaru <101541944+wattaraaru@users.noreply.github.com>
2023-02-12 22:14:29 +0900

    カルタゲーム一連の実装

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
+import { Modal } from "./compornents/Modal";
 import { useInterval } from "./useInterval";
+import "./App.css";
 
 
 const CommandKaruta = () => {
@@ -58,7 +60,7 @@ const CommandKaruta = () => {
   const [efudaLists, setEfudaLists] = useState(yomiLists); //絵札管理
   const [userScore, setUserScore] = useState(0); //ユーザーのスコア管理
   const [cpuScore, setCpuScore] = useState(0); //CPUのスコア管理
-  const [isModalOpen, setModalOpen] = useState(false); //正誤判定後のモーダル管理
+  const [isModalOpen, setIsModalOpen] = useState(false); //正誤判定後のモーダル管理
   const [isStarted, setIsStarted] = useState(false); //ゲーム開始ボタンの管理
   const [isKaruta, setIsKaruta] = useState(false); //絵札一覧の表示・非表示の管理
   const [isRunning,setIsRunning] = useState(false);
@@ -68,6 +70,19 @@ const CommandKaruta = () => {
   const [currentTurn, setCurrentTurn] = useState(0); //ターンカウント
   const [userAcquiredCards, setUserAcquiredCards] = useState([]); //ユーザーが取得した絵札の管理
   const [cpuAcquiredCards, setCpuAcquiredCards] = useState([]); //CPUが取得した絵札の管理
+
+  //初期化
+  const initGame = () => {
+    setUserScore(0);
+    setCpuScore(0);
+    setUserAcquiredCards([]);
+    setCpuAcquiredCards([]);
+    setIsAnswered([]);
+    setCurrentTurn(0);
+    setIsKaruta(false);
+    setIsStarted(false);
+    setIsModalOpen(false);
+  };
 
   //ゲームスタート
   const startGame = () => {
@@ -88,13 +103,24 @@ const CommandKaruta = () => {
   };
 
   //3秒待つ
+  // useEffect(() => {
+  //   if (!isStarted || currentTurn >= 9) return;
+  //   setTimeout(() => {
+  //     setIsKaruta(true);
+  //     setOneCharactorQuestion(yomiLists[currentTurn].question);
+  //     setIsRunning(true);
+  //   }, 1000);
+  //   // eslint-disable-next-line
+  // }, [isStarted, currentTurn]);
   useEffect(() => {
+    const delay = currentTurn === 0 ? 3000 : 500;
     if (!isStarted || currentTurn >= 9) return;
     setTimeout(() => {
-      setIsKaruta(true);
+      setShowQuestion("");
       setOneCharactorQuestion(yomiLists[currentTurn].question);
       setIsRunning(true);
-    }, 1000);
+      setIsKaruta(true);
+    }, delay);
     // eslint-disable-next-line
   }, [isStarted, currentTurn]);
 
@@ -117,27 +143,6 @@ const CommandKaruta = () => {
     isRunning ? 100 : null
   );
 
-  //ユーザーがクリックした札に対する正誤判定
-
-  //スコアの更新
-
-  //正誤判定に対応したモーダルの表示
-
-  //スコア表示
-  //取得した（された）絵札を非表示にする
-
-  //モーダルは2秒後に消える
-
-  //次の問題へ移る
-
-  //次の読み札を表示する
-
-  //最終問題までループする
-
-  //勝敗とスコア、ホーム画面へ飛ぶボタンが表示してあるのモーダル表示
-
-
-  //関数
   //シャッフル関数
   const shuffle=(arr) => {
     for (let i = arr.length - 1; i > 0; i--) {
@@ -150,35 +155,52 @@ const CommandKaruta = () => {
   const judge = (clickTarget) => {
     console.log(clickTarget);
     const index = efudaLists.findIndex((efuda) => efuda.id === yomiLists[currentTurn].id)
-    const isAnswerdCopy = isAnswered.slice();
-    isAnswerdCopy.push(efudaLists[index].id);
-    setIsAnswered(isAnswerdCopy);
+    const newIsAnswerd = isAnswered.slice();
+    newIsAnswerd.push(efudaLists[index].id);
+    setIsAnswered(newIsAnswerd);
     if(clickTarget.id === yomiLists[currentTurn].id){
       setUserScore(userScore+1);
 
       const tempStorage = efudaLists[index].answer;
-      const acquiredCardCopy = [...userAcquiredCards];
-      acquiredCardCopy.push(tempStorage);
-      setUserAcquiredCards(acquiredCardCopy);
-      console.log("ok");
+      const newAcquiredCard = [...userAcquiredCards];
+      newAcquiredCard.push(tempStorage);
+      setUserAcquiredCards(newAcquiredCard);
     }else{
       setCpuScore(cpuScore+1);
 
       const tempCpuStorage = efudaLists[index].answer;
-      const cpuAcquiredCardCopy = [...cpuAcquiredCards];
-      cpuAcquiredCardCopy.push(tempCpuStorage);
-      setCpuAcquiredCards(cpuAcquiredCardCopy);
-      console.log("no");
+      const newCpuAcquiredCard = [...cpuAcquiredCards];
+      newCpuAcquiredCard.push(tempCpuStorage);
+      setCpuAcquiredCards(newCpuAcquiredCard);
     }
   }
 
   const efudaClick = (e) => {
     judge(e.target);
-    //次の問題へ
-    setCurrentTurn(currentTurn+1);
+    modalOpen();
   }
 
   //問題文の表示
+  //モーダルを表示
+  const modalOpen = () => {
+    setIsModalOpen(true);
+  }
+
+  //モーダルを閉じる
+  const modalClose = () => {
+    if (currentTurn < 8) {
+      setIsModalOpen(false);
+      setCurrentTurn((prev) => prev + 1);
+    } else if (currentTurn === 8) {
+      finishGame();
+    }
+  };
+
+  //勝敗とスコア、ホーム画面へ飛ぶボタンが表示してあるのモーダル表示
+  const finishGame = () => {
+    setCurrentTurn(currentTurn + 1);
+    setIsModalOpen(true);
+  };
 
 
   return (
@@ -194,7 +216,7 @@ const CommandKaruta = () => {
         {isKaruta && (
         <div>
           <div>
-            {/* 読み札の表示 */}
+            <p>{showQuestion}</p>
           </div>
           <div>
             <p>CPUスコア：{cpuScore}</p>
@@ -217,6 +239,15 @@ const CommandKaruta = () => {
             <p>取得した札：{userAcquiredCards}</p>
           </div>
         </div>
+        )}
+        {isModalOpen ? (
+          currentTurn < 9 ? (
+            <Modal modalClose={modalClose}>問題の解説</Modal>
+          ) : (
+            <Modal modalClose={initGame}>終わり</Modal>
+          )
+        ) : (
+          ""
         )}
       </div>
     </div>
