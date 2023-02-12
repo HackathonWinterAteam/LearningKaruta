fbe4871e6c7bdd112cf6ebf055cfa162cc252c5f
maimy <m.711029@gmail.com>
2023-02-12 23:21:19 +0900

    札取得処理（最低限）

--- app/cruds/games.py
+++ app/cruds/games.py
@@ -19,13 +19,13 @@ def read_boxes(db: Session, skip: int = 0):
 
 
 def read_playing_cards(db: Session, box_id: int) -> games_model.boxes_cards :
-    bid = games_model.boxes_cards.box_id
+    # bid = games_model.boxes_cards.box_id
     t = text('SELECT qac.card_id AS card_id, qt.question_id AS question_id, \
             at.answer_id AS answer_id, qt.question_text AS question_text, \
             at.answer_text AS answer_text, aimg.answer_file_pass AS answer_file_pass \
             FROM (box_card AS bc \
             INNER JOIN question_answer_cards AS qac \
-            ON bc.card_id = qac.card_id AND bc.box_id = :box_id \
+            ON bc.card_id = qac.card_id AND bc.box_id = box_id \
             INNER JOIN question_texts AS qt \
             ON qac.question_id = qt.question_id) \
             INNER JOIN answer_texts AS at \
@@ -33,51 +33,12 @@ def read_playing_cards(db: Session, box_id: int) -> games_model.boxes_cards :
             LEFT JOIN answer_images AS aimg \
             ON at.answer_id = aimg.answer_id' \
             ) #結合抽出処理
-    t = t.bindparams(bindparam('box_id', value='bid', type_=Integer))
-    results = db.execute(t)
-    for result in results:
-        # 一行ずつリスト化
-        PlayCardsList = list(result)
+    # t = t.bindparams(bindparam('box_id', value='bid', type_=Integer))
+    PlayCardsList = db.execute(t).fetchall()
 
-        # valueをint化して配列にpush
-        # card_id.push(str(PlayCardsList['card_id']))
-
-        return PlayCardsList
+    return PlayCardsList
 
 
-
-# お題一覧取得
-
-def read_boxes(db: Session, skip: int = 0):
-    BoxList = db.query(games_model.boxes).offset(skip).all()
-    return BoxList
-
-
-def read_playing_cards(db: Session, box_id: int) -> games_model.boxes_cards :
-    bid = games_model.boxes_cards.box_id
-    t = text('SELECT qac.card_id AS card_id, qt.question_id AS question_id, \
-            at.answer_id AS answer_id, qt.question_text AS question_text, \
-            at.answer_text AS answer_text, aimg.answer_file_pass AS answer_file_pass \
-            FROM (box_card AS bc \
-            INNER JOIN question_answer_cards AS qac \
-            ON bc.card_id = qac.card_id AND bc.box_id = :box_id \
-            INNER JOIN question_texts AS qt \
-            ON qac.question_id = qt.question_id) \
-            INNER JOIN answer_texts AS at \
-            ON qac.answer_id = at.answer_id \
-            LEFT JOIN answer_images AS aimg \
-            ON at.answer_id = aimg.answer_id' \
-            ) #結合抽出処理
-    t = t.bindparams(bindparam('box_id', value='bid', type_=Integer))
-    results = db.execute(t)
-    for result in results:
-        # 一行ずつリスト化
-        PlayCardsList = list(result)
-
-        # valueをint化して配列にpush
-        # card_id.push(str(PlayCardsList['card_id']))
-
-        return PlayCardsList
 
 
 
