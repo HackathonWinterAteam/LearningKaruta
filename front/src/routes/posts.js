293f98f06f3ea1d796e7670322bee9468a4f9294
harumakin <shirasaka0116@gmail.com>
2023-02-13 23:02:25 +0900

    index.jsで読み込んだデータをmapで取得する。

--- front/src/routes/posts.js
+++ front/src/routes/posts.js
@@ -1,3 +1,4 @@
+import { useLoaderData } from "react-router-dom";
 export async function loader() {
   const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
   const posts = await res.json();
@@ -5,9 +6,17 @@ export async function loader() {
 }
 
 function Posts() {
+  const { posts } = useLoaderData();
   return (
     <>
       <h2>Posts</h2>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>
+            {post.id}:{post.title}
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
