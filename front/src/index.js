3cb189adc519c7cddce4a11ec8d3ae650118f237
harumakin <shirasaka0116@gmail.com>
2023-02-15 09:05:44 +0900

    ルーティング設定をしている親元のファイルRootを読み込むように変更

--- front/src/index.js
+++ front/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import Main from "./Main";
+import Root from "./routes/root";
 import { BrowserRouter } from "react-router-dom";
 // import App from "./App";
 
@@ -17,7 +17,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Main />
+      <Root />
     </BrowserRouter>
   </React.StrictMode>
 );
