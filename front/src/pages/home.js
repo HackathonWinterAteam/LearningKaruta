01ca9164bd9168e28a8da886b24bdcc499155185
harumakin <shirasaka0116@gmail.com>
2023-02-15 09:19:47 +0900

    ユーザーのデータをバックエンドから取得するための処理を記述

--- front/src/pages/home.js
+++ front/src/pages/home.js
@@ -1,9 +1,34 @@
+import axios from "../utils/axios";
+import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 
 function Home() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const getUsers = async () => {
+      try {
+        // バックエンドからユーザーのデータを取得する
+        const response = await axios.get("/users");
+        setUsers(response.data.users);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getUsers();
+  }, []);
+
   return (
     <>
       <h2>Homeだよ</h2>
+      <ul>
+        {users &&
+          users.map((user) => (
+            <li key={user._id}>
+              Name:{user.name}/Email:{user.email}
+            </li>
+          ))}
+      </ul>
 
       <li>
         <NavLink
