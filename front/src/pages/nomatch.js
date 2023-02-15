c39fe9b26a874d592aa6f8d5f778f0f7357b5b96
harumakin <shirasaka0116@gmail.com>
2023-02-15 09:09:26 +0900

    404ページを作成。ファイル名は要検討

--- front/src/pages/nomatch.js
+++ front/src/pages/nomatch.js
@@ -1,5 +1,20 @@
+import { NavLink } from "react-router-dom";
+
 function NoMatch() {
-  return <h2>このページは存在しません。</h2>;
+  return (
+    <>
+      <h2>このページは存在しません。</h2>
+
+      <li>
+        <NavLink
+          style={({ isActive }) => (isActive ? { color: "blue" } : undefined)}
+          to="/"
+        >
+          Home
+        </NavLink>
+      </li>
+    </>
+  );
 }
 
 export default NoMatch;
