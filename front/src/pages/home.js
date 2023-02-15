9049527bbe740cf2942eff51ba370019ccfeb1ff
harumakin <shirasaka0116@gmail.com>
2023-02-15 09:07:38 +0900

    仮のホーム画面の実装

--- front/src/pages/home.js
+++ front/src/pages/home.js
@@ -1,5 +1,36 @@
+import { NavLink } from "react-router-dom";
+
 function Home() {
-  return <h2>Home</h2>;
+  return (
+    <>
+      <h2>Homeだよ</h2>
+
+      <li>
+        <NavLink
+          style={({ isActive }) => (isActive ? { color: "blue" } : undefined)}
+          to="/"
+        >
+          Home
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          style={({ isActive }) => (isActive ? { color: "blue" } : undefined)}
+          to="/signup"
+        >
+          Signup
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          style={({ isActive }) => (isActive ? { color: "blue" } : undefined)}
+          to="/login"
+        >
+          Login
+        </NavLink>
+      </li>
+    </>
+  );
 }
 
 export default Home;
