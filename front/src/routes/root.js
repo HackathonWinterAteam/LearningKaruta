9d2440d165e16b36b3d26eb02540278bfb1019cf
harumakin <shirasaka0116@gmail.com>
2023-02-15 09:06:53 +0900

    rootでルーティング設定をする。

--- front/src/routes/root.js
+++ front/src/routes/root.js
@@ -1,24 +1,59 @@
-import { Link, Outlet } from "react-router-dom";
-const Root = () => {
+import { Routes, Route, NavLink } from "react-router-dom";
+
+import Home from "../pages/home";
+import NoMatch from "../pages/nomatch";
+import Signup from "../pages/signup";
+import Login from "../pages/login";
+import Karuta from "../pages/karuta";
+
+function Root() {
   return (
-    <>
-      <ul>
+    <div className="App">
+      {/* <h1>コマンドカルタ</h1> */}
+      <ul className="flex flex-row justify-center ">
         <li>
-          <Link to="/">Home</Link>
+          <NavLink
+            style={({ isActive }) => (isActive ? { color: "blue" } : undefined)}
+            to="/"
+          >
+            Home
+          </NavLink>
         </li>
         <li>
-          <Link to="/about">About</Link>
+          <NavLink
+            style={({ isActive }) => (isActive ? { color: "blue" } : undefined)}
+            to="/signup"
+          >
+            Signup
+          </NavLink>
         </li>
         <li>
-          <Link to="/contact">Contact</Link>
+          <NavLink
+            style={({ isActive }) => (isActive ? { color: "blue" } : undefined)}
+            to="/login"
+          >
+            Login
+          </NavLink>
         </li>
+
         <li>
-          <Link to="/posts">Posts</Link>
+          <NavLink
+            style={({ isActive }) => (isActive ? { color: "blue" } : undefined)}
+            to="/karuta"
+          >
+            Karuta
+          </NavLink>
         </li>
       </ul>
-      <Outlet />
-    </>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/karuta" element={<Karuta />} />
+        <Route path="*" element={<NoMatch />} />
+      </Routes>
+    </div>
   );
-};
+}
 
 export default Root;
