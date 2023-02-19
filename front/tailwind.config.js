29844196719431a3db8b5369a875791b4ef17bbf
Natsuki0816 <happi68end@gmail.com>
2023-02-19 21:49:27 +0900

    勝利画面の実装

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        body: [
+          '"Noto sans JP"',
+        ]
+      },
+      colors:{
+        lightBlue:'#3F4CE2',
+        darkBlue:'#021997',
+        yellow:'#F1D772',
+        darkWhite:'#F5F4F5',
+      },
+      dropShadow:{
+        btnShadow:'0 8px 8px rgba(2, 25, 151,30)'
+      }
+    },
   },
   plugins: [],
 };
