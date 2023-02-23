72ed1653997ded8a73a6be4cd4cb37aab91df57f
watttaraaru <101541944+wattaraaru@users.noreply.github.com>
2023-02-23 21:20:27 +0900

    絵札の表示を5*2に変更

--- front/src/pages/karuta.js
+++ front/src/pages/karuta.js
@@ -193,31 +193,36 @@ const MKaruta = () => {
       <div>
         {isKaruta && (
           <div>
-            <div className="text-3xl text-center mb-3 bg-gray-300 ">
-              <p>{showQuestion}</p>
+            <div className="text-center bg-gray-100 h-[52px]">
+              <p className="text-3xl py-2 ">{showQuestion}</p>
             </div>
-            <div className="fixed left-0 top-0 w-20 h-20 rounded-lg bg-blue-500 text-yellow-300 text-5xl text-center leading-normal outline-none">
-              <p>{cpuScore}</p>
-              <p className="text-2xl mt-2">{cpuAcquiredCards}</p>
-            </div>
-            <ul className="flex flex-wrap mt-20 ml-32 ">
-              {currentTurn < 9 &&
-                efudaLists
-                  .filter((answeredElm) => !isAnswered.includes(answeredElm.card_id))
-                  .map((efuda) => (
-                    <div className=" mx-24 mb-10 w-40" key={efuda.card_id}>
-                      <img
-                        alt="画像"
-                        src={`${process.env.PUBLIC_URL}/imgs/${efuda.answer_file_pass}`}
-                        onClick={(e) => efudaClick(e)}
-                        id={efuda.id}
-                      />
-                    </div>
-                  ))}
-            </ul>
-            <div className="fixed right-0 bottom-0 w-20 h-20 rounded-lg bg-yellow-300 text-blue-500 text-5xl text-center leading-normal outline-none">
-              <p>{userScore}</p>
-              <p className="text-2xl">{userAcquiredCards}</p>
+            <div className="w-full h-[calc(100vh-100px)] text-center m-auto relative">
+              <div className="absolute left-0 top-0 w-20 h-20 rounded-lg bg-blue-500 text-yellow text-5xl text-center leading-normal outline-none">
+                <p>{cpuScore}</p>
+                {/* <p className="text-2xl mt-2">{cpuAcquiredCards}</p> */}
+              </div>
+              <ul className="w-[95%] h-3/5 absolute left-[3%] top-[16%] box-border grid grid-cols-5 grid-rows-2 gap-y-7">
+                {currentTurn < 9 &&
+                  efudaLists.map((efuda) =>
+                    !isAnswered.includes(efuda.id) ? (
+                      <div key={efuda.id} className="flex justify-center">
+                        <img
+                          alt="画像"
+                          className="object-contain w-4/5"
+                          src={`${process.env.PUBLIC_URL}/imgs/${efuda.answer}`}
+                          onClick={(e) => efudaClick(e)}
+                          id={efuda.id}
+                        />
+                      </div>
+                    ) : (
+                      <div></div>
+                    )
+                  )}
+              </ul>
+              <div className="absolute right-0 bottom-0 w-20 h-20 rounded-lg bg-yellow text-blue-500 text-5xl text-center leading-normal outline-none">
+                <p>{userScore}</p>
+                {/* <p className="text-2xl">{userAcquiredCards}</p> */}
+              </div>
             </div>
           </div>
         )}
